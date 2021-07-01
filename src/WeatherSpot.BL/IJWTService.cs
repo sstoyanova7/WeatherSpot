c50b91567a0083759b0d88d6a4efb073b3f48b1c
Simona Stoyanova <s.stoyanova@draftkings.com>
2021-07-01 23:11:20 +0300

    typo

--- src/WeatherSpot.BL/IJWTService.cs
+++ src/WeatherSpot.BL/IJWTService.cs
@@ -5,8 +5,8 @@ namespace WeatherSpot.BL
 {
 
     public interface IJWTService
-    {      
-        string GenerateJSONWebToken(UserLoginRequestModel userInfo);
+    {
+        string GenerateJSONWebToken(UserLoginRequestModel userInfo, out UserModel user);
         UserModel DecodeJWTAsync(string jwt);
     }
 }
--- src/WeatherSpot.BL/IUserService.cs
+++ src/WeatherSpot.BL/IUserService.cs
@@ -11,8 +11,10 @@
         public IEnumerable<StationsResponseMoedl> GetRoles();
         public IEnumerable<UserModel> GetUsers();
         public ResponseWithMessage CreateUser(NewUserRequestModel newUser);
-        public ResponseWithMessage ChangePassword(string newPassword);
-        public ResponseWithMessage ChangeUserRole(ChangeUserRoleRequestModel requestModel);
+        public ResponseWithMessage ChangePassword(ChangePasswordRequestModel request);
+        public ResponseWithMessage ChangeUserRole(ChangeUserRoleRequestModel request);
+        public ResponseWithMessage ChangeUsername(ChangeUsernameRequestModel request);
         public ResponseWithMessage DeactivateUser(int userId);
+        public ResponseWithMessage ActivateUser(int userId);
     }
 }
