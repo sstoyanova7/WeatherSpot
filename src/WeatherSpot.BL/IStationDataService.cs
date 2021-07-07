83760893252d04ab5e3a37cfc9c2a5477f9ed371
Simona Stoyanova <s.stoyanova@draftkings.com>
2021-07-07 13:39:24 +0300

    sql fix

--- src/WeatherSpot.BL/IStationDataService.cs
+++ src/WeatherSpot.BL/IStationDataService.cs
@@ -6,7 +6,7 @@ namespace WeatherSpot.BL
 {
     public interface IStationDataService
     {
-        public IEnumerable<StationData> GetStationData(int regionId, int cityId, int month, int year);
+        public IEnumerable<StationData> GetStationData(int cityId, int month, int year);
         public ResponseWithMessage AddStationData(StationDataRequestModel request);
         public ResponseWithMessage UpdateStationData(UpdateStationDataRequestModel request);
         public ResponseWithMessage DeleteStationData(int stationDataId);    
